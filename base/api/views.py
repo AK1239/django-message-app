2a9f8e903fe5d050966dba7f26a3c5db5e6e71ab
AK1239 <akilkhatri235@gmail.com>
2024-10-30 13:19:26 +0300

    final commit

--- base/api/views.py
+++ base/api/views.py
@@ -1,16 +1,17 @@
-from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from base.models import Room
 from .serializers import RoomSerializer
+from base.api import serializers
+
 
 @api_view(['GET'])
 def getRoutes(request):
     routes = [
         'GET /api',
         'GET /api/rooms',
-        'GET /api/rooms/:id',
+        'GET /api/rooms/:id'
     ]
-
     return Response(routes)
 
 
@@ -19,6 +20,7 @@ def getRooms(request):
     rooms = Room.objects.all()
     serializer = RoomSerializer(rooms, many=True)
     return Response(serializer.data)
+
 
 @api_view(['GET'])
 def getRoom(request, pk):
